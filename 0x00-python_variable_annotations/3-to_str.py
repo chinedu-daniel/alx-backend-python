f0d705e75d0533dff7424b084d081c97c5fe6f1b
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-16 19:27:50 +0100

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Basic annotation - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    function to take a float anf return a string
+    """
+    return str(n)
