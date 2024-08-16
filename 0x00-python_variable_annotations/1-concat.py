c766b316bb1da71b7bdeac0cdec5cac1c78a40be
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-08-16 19:06:56 +0100

    1-concat.py

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,8 +4,8 @@ Basic annotation function
 """
 
 
-def concat(str1: string, str2: string) -> None:
+def concat(str1: str, str2: str) -> str:
     """
     function to concatenate two strings
     """
-    return "str1" + "" + "str2"
+    return str1 + str2
