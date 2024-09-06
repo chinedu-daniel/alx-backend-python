3bb1e5a9a4a348603e57d78da0750edf44d26dac
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-06 20:16:15 +0100

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,11 +16,11 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self):
+    def test_access_nested_map(self, nested_map, path, expected):
         """
         method to test that the method returns what it is supposed to
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected_value)
+        self.assertEqual(access_nested_map(nested_map, path=path), expected)
 
 
 if __name__ == "__main__":
