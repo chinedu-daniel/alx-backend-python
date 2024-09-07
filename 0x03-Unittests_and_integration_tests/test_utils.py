81a8ea032d12b9aa94b27a534722d7c0799fa197
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-07 14:03:25 +0100

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@ Parameterize a unit test
 """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -21,6 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
         method to test that the method returns what it is supposed to
         """
         self.assertEqual(access_nested_map(nested_map, path=path), expected)
+
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
@@ -33,6 +35,31 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Mock HTTP calls
+    """
+    @patch('utils.requests.get')
+    @parameterized.expand([
+        test_url="http://example.com", test_payload={"payload": True}
+        test_url="http://holberton.io", test_payload={"payload": False}
+    ])
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        To test that utils.get_json returns the expected result
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
