81389dcb85485f6ed808ed4dd173cffcf55d6942
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2024-09-07 14:14:49 +0100

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -46,7 +46,7 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
-    def test_get_json(self, test_url, test_payload, mock_get):
+    def test_get_json(self, url, payload, mock_get):
         """
         To test that utils.get_json returns the expected result
         """
